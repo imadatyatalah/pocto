85382c004fc6cfc91728935e8c792a6165ed50c3
imad <imadatyatalah@gmail.com>
2022-01-29 15:38:03 +0100

    feat(app-server): Added ban user route #3
    - feat(app-admin): Added UserCard component & useBanUser hook

---
+++ apps/admin/src/components/UserCard/UserCard.tsx
@@ -0,0 +1,26 @@
+import { Heading, Flex, Button } from "ui";
+
+import { useBanUser } from "@/mutations/index";
+
+const UserCard = ({ user }) => {
+  const { name, username, isBanned } = user;
+
+  const { mutate: banUser } = useBanUser(username);
+
+  const handleBanUser = () => banUser();
+
+  return (
+    <Flex justify="between" align="center">
+      <div>
+        <Heading>{name}</Heading>
+        <p>{username}</p>
+      </div>
+
+      <Button variant="danger" onClick={handleBanUser} disabled={isBanned}>
+        {isBanned ? "Banned" : "Ban user"}
+      </Button>
+    </Flex>
+  );
+};
+
+export default UserCard;
--- apps/server/src/controllers/admin/users.controllers.ts
+++ apps/server/src/controllers/admin/users.controllers.ts
@@ -1,13 +1,29 @@
 import { Request, Response } from "express";
 
 import { prisma } from "../../config/prisma";
-import { userData } from "../../helpers/users";
+import { findUserByUsername, userData } from "../../helpers/users";
 
 export const getUsers = async (req: Request, res: Response) => {
   try {
     const users = await prisma.user.findMany({ select: userData });
 
     res.status(200).send(users);
+  } catch (err) {
+    res
+      .status(500)
+      .send({ success: false, message: "Something went wrong", error: err });
+  }
+};
+
+export const banUser = async (req: Request, res: Response) => {
+  try {
+    const bannedUser = await prisma.user.update({
+      where: findUserByUsername(req.params.username),
+      data: { isBanned: true },
+      select: userData,
+    });
+
+    res.status(200).send(bannedUser);
   } catch (err) {
     res
       .status(500)
